ba869a6a29a0c0cdc16cb01638ae3285bd714d2d
Sean Sáez Fuller <sean.saez.f@gmail.com>
2022-10-05 15:00:13 +0200

    Seccion 9 finalizada - gestor usuarios

---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import "./input.css"
+
+const Input = ({ label, ...rest }) => {
+
+  return (
+    <div className="field">
+      <label>{label}</label>
+      <input {...rest} />
+    </div>
+  )
+}
+
+export default Input
